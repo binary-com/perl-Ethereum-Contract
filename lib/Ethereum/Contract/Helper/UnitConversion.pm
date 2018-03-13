0fc850d0d503eb86cde22caa0e5c2a3b412715e7
regcostajr <regcostajr@gmail.com>
2018-03-13 05:01:30 +0000

    unit conversion number

--- lib/Ethereum/Contract/Helper/UnitConversion.pm
+++ lib/Ethereum/Contract/Helper/UnitConversion.pm
@@ -9,16 +9,16 @@ use Math::BigInt;
     Ethereum::Contract::Helper::UnitConversion - Ethereum Unit Converter
     
     wei:        ‘1’
-    kwei:       ‘1000’
-    mwei:       ‘1000000’
-    gwei:       ‘1000000000’
-    szabo:      ‘1000000000000’
-    finney:     ‘1000000000000000’
-    ether:      ‘1000000000000000000’
-    kether:     ‘1000000000000000000000’
-    mether:     ‘1000000000000000000000000’
-    gether:     ‘1000000000000000000000000000’
-    tether:     ‘1000000000000000000000000000000’
+    kwei:       ‘1E3’
+    mwei:       ‘1E6’
+    gwei:       ‘1E9’
+    szabo:      ‘1E12’
+    finney:     ‘1E15’
+    ether:      ‘1E18’
+    kether:     ‘1E21’
+    mether:     ‘1E24’
+    gether:     ‘1E27’
+    tether:     ‘1E30’
 
 =cut
 
@@ -27,43 +27,43 @@ sub to_wei {
 }
 
 sub to_kwei {
-    return to_hex(shift, 1000);
+    return to_hex(shift, 1E3);
 }
 
 sub to_mwei {
-    return to_hex(shift, 1000000);
+    return to_hex(shift, 1E6);
 }
 
 sub to_gwei {
-    return to_hex(shift, 1000000000);
+    return to_hex(shift, 1E9);
 }
 
 sub to_szabo {
-    return to_hex(shift, 1000000000000);
+    return to_hex(shift, 1E12);
 }
 
 sub to_finney {
-    return to_hex(shift, 1000000000000000);
+    return to_hex(shift, 1E15);
 }
 
 sub to_ether {
-    return to_hex(shift, 1000000000000000000);
+    return to_hex(shift, 1E18);
 }
 
 sub to_kether {
-    return to_hex(shift, 1000000000000000000000);
+    return to_hex(shift, 1E21);
 }
 
 sub to_mether {
-    return to_hex(shift, 1000000000000000000000000)
+    return to_hex(shift, 1E24)
 }
 
 sub to_gether {
-    return to_hex(shift, 1000000000000000000000000000);
+    return to_hex(shift, 1E27);
 }
 
 sub to_tether {
-    return to_hex(shift, 1000000000000000000000000000000);
+    return to_hex(shift, 1E30);
 }
 
 sub to_hex {
